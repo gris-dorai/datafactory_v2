80a265763b8b57029bb72fbae1c4ee943ee5153c
gris-dorai <126446424+gris-dorai@users.noreply.github.com>
2023-03-07 21:03:10 +0530

    Update datafactory.py

--- dags/models/datafactory.py
+++ dags/models/datafactory.py
@@ -17,6 +17,9 @@ from services.db_service import engine, Session
 if not engine.dialect.has_schema(engine, "datafactory"):
     engine.execute(schema.CreateSchema("datafactory"))
 
+conn = engine.connect()
+conn.execute("CREATE EXTENSION postgis")    
+    
 Base = declarative_base()
 
 df_session = Session()
